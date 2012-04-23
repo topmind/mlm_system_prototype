b70c54d686f32bf89627be83bebe57108c65fc51
Steven Gao <topmind@gmail.com>
2012-04-23 12:57:46 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MlmSystemPrototype::Application.config.secret_token = '60281f8ca4825d738a7ccf320777189f6d7a5285b4e539ddf5c3690c7b1500b9ef4e7d038d822cfcef60143dd204afa89478000f287a3eb6aaf327c066bfac80'
